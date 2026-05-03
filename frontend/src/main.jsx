927603de033286349cbab4829087f5038e88038b
aloknox <143309447+alokkksharmaa@users.noreply.github.com>
2026-05-03 23:11:50 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import axis from 'Axios'
+import axios from 'Axios'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
